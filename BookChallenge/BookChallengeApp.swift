44aaa89a290abbef0ee8a250409bcb32e04588a4
John <johnathanlee72@hotmail.com>
2022-04-18 22:33:45 -0700

    Initial Commit

---
+++ BookChallenge/BookChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookChallengeApp.swift
+//  BookChallenge
+//
+//  Created by Johnathan Lee on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct BookChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
